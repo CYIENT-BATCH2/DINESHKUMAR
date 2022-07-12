e59d766c86868ec53c7f21648ef46b06a32f4717
DINESHKUMARK0417 <108974294+DINESHKUMARK0417@users.noreply.github.com>
2022-07-12 23:49:09 +0530

    Update and rename SUM,AVG.c to W1_D3_Q1 SUM,AVG.c

--- WEEK1/DAY3/W1_D3_Q1 SUM,AVG.c
+++ WEEK1/DAY3/W1_D3_Q1 SUM,AVG.c
@@ -5,9 +5,11 @@ int main() {
 int i1,i2,i3,i4,i5,i6,i7,i8,d=0,isum=0,iavg=0,imean=0,min=0,max=0;
 float f1,f2,f3,f4,f5,f6,f7,fsum=0,favg=0,fmean=0;
 scanf("%d%d%d%d%d%d%d%d%f%f%f%f%f%f%f",&i1,&i2,&i3,&i4,&i5,&i6,&i7,&i8,&f1,&f2,&f3,&f4,&f5,&f6,&f7);
-isum=i1+i2+i3+i4+i5+i6+i7+i8;
-iavg=isum/8;
-imean=iavg;
+
+    
+    isum=i1+i2+i3+i4+i5+i6+i7+i8;
+    iavg=isum/8;
+    imean=iavg;
 printf("sum =%d mean=%d average =%d",isum,imean,iavg);
 
 fsum=(f1+f2+f3+f4+f5+f6+f7);
