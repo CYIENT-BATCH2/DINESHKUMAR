94f0343adbde636394158e58d255aa52e6381454
DINESHKUMARK0417 <108974294+DINESHKUMARK0417@users.noreply.github.com>
2022-07-12 19:48:54 +0530

    Update q1 %11.c

--- WEEK1/DAY1/q1 %11.c
+++ WEEK1/DAY1/q1 %11.c
@@ -1,4 +1,14 @@
-// Online C compiler to run C program online
+/*
+steps:
+
+1.read data 
+2.substract one from given data   -->   (data-1)
+3.modulus operation with 11 -->  (data-1)%11
+4.((n-1)%11)&&(n>0) data be +ve and divisible by 11 after substract 
+5.printing if condition true as  special 
+
+*/
+
 #include <stdio.h>
 
 int main() {
