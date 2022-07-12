8aca3d6dbee41579943c381227c1a97cdd23cd0b
DINESHKUMARK0417 <108974294+DINESHKUMARK0417@users.noreply.github.com>
2022-07-12 19:33:24 +0530

    Add files via upload

---
+++ WEEK1/DAY2/W1D1Q4 CONTAINS 3 IN NUMBER.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+int n1,n2,r=0,k=0,s,t=0;
+scanf("%d %d",&n1,&n2);
+l:
+if(n1<=n2)
+{k=n1;
+s=n1;
+whl:
+if(s!=0)
+{
+    r=s%10;
+    if(r==3){
+     printf(" %d",k);  
+        goto out;
+    }s/=10; 
+    goto whl;
+}
+out:
+n1++;
+goto l;
+}
+}
+
+
