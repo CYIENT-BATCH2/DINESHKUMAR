1bae5781fd55723d48c70c65ee74313766560e4c
DINESHKUMARK0417 <108974294+DINESHKUMARK0417@users.noreply.github.com>
2022-07-12 19:52:01 +0530

    Update q10 which is grater.c

--- WEEK1/DAY1/q10 which is grater.c
+++ WEEK1/DAY1/q10 which is grater.c
@@ -1,3 +1,11 @@
+/*
+steps:
+1.read 3 values
+2.checking which is grater 
+3. if n1 is greater than both n2 and n3, n1 is the largest.
+4.if n2 is greater than both n1 and n3, n2 is the largest.
+5.if n3 is greater than both n1 and n2, n3 is the largest
+6.prints which is grater.
 #include <stdio.h>
 
 int main() {
@@ -7,15 +15,12 @@ int main() {
   printf("Enter three different numbers: ");
   scanf("%lf %lf %lf", &n1, &n2, &n3);
 
-  // if n1 is greater than both n2 and n3, n1 is the largest
   if (n1 >= n2 && n1 >= n3)
     printf("%.2f is the largest number.", n1);
 
-  // if n2 is greater than both n1 and n3, n2 is the largest
   if (n2 >= n1 && n2 >= n3)
     printf("%.2f is the largest number.", n2);
 
-  // if n3 is greater than both n1 and n2, n3 is the largest
   if (n3 >= n1 && n3 >= n2)
     printf("%.2f is the largest number.", n3);
 
