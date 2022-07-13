d534484e47a59c0447e9e9d2064c7e13425d8904
DINESHKUMARK0417 <108974294+DINESHKUMARK0417@users.noreply.github.com>
2022-07-13 10:03:26 +0530

    Update W1_D4_Q3.c

--- WEEK1/DAY4/W1_D4_Q3.c
+++ WEEK1/DAY4/W1_D4_Q3.c
@@ -1,15 +1,20 @@
-(C)LOGICAL OPERATOR
+
+/*  (C)LOGICAL OPERATOR
+ALGORITHM:
+steps 
+1.apply the expression as question given .
+*/
 #include <stdio.h>
 int main() {
-    // int i=10,j=20,k;
-    // printf("value of k=%d\n",i&&j);
-    // printf("value of k=%d\n",i||j);
-    // printf("value of k=%d\n",k=!i);
-    //  int i=10,j=0,k;
-    // printf("value of k=%d\n",i&&(j=20));
-    // printf("value of k=%d\n",i||(j=20));
-    //  int i=0,j=0,k=30,l;
-    // printf("value of l=%d\n",i||(j=20)&&k);
+    int i=10,j=20,k;
+   printf("value of k=%d\n",i&&j);
+   printf("value of k=%d\n",i||j);
+    printf("value of k=%d\n",k=!i);
+     int i=10,j=0,k;
+    printf("value of k=%d\n",i&&(j=20));
+    printf("value of k=%d\n",i||(j=20));
+     int i=0,j=0,k=30,l;
+    printf("value of l=%d\n",i||(j=20)&&k);
      int i=10,j=20,k=0,l;
     printf("value of l=%d\n",i&&j||j&&k);
     printf("value of l=%d\n",i||j&&i||k);
