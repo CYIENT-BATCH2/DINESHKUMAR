94905d89eae6aea57e8458f40da0a9f66333db03
DINESHKUMARK0417 <108974294+DINESHKUMARK0417@users.noreply.github.com>
2022-07-26 15:54:53 +0530

    Update day8 Q4 functions.c

--- WEEK2/DAY8/day8 Q4 functions.c
+++ WEEK2/DAY8/day8 Q4 functions.c
@@ -1,4 +1,9 @@
 // Online C compiler to run C program online
+/*
+1.read two variables named as byte and bit
+2.take a function with two arguments.first argument should be byte number and second argument will be bit position number.
+3.clear the bit position in corresponding byte as per user input.
+*/
 #include <stdio.h>
 void fun(int a,int b);
   int num=0x12345678;
