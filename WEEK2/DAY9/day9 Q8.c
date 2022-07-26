f154b371cbe80dcf5fe981993db6ced477c392ca
DINESHKUMARK0417 <108974294+DINESHKUMARK0417@users.noreply.github.com>
2022-07-26 16:03:59 +0530

    Update day9 Q8.c

--- WEEK2/DAY9/day9 Q8.c
+++ WEEK2/DAY9/day9 Q8.c
@@ -1,4 +1,9 @@
 // Online C compiler to run C program online
+/*ALGORITHM
+1.read the variables as ADC0N0 and CMCON
+2.set the bit positions of 1,4 and 7.
+3.find the bit positions of 4,7 and 8 in ADCONO.
+*/
 #include <stdio.h>
 int main() {
     // Write C code here
